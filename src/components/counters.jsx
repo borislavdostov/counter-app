27f98a17460129922940ef677650940b5b535183
Borislav Dostov <bdostov@gmail.com>
2021-10-25 17:31:40 +0300

    Improvements

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,28 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Counter from './counter';
 
-class Counters extends React.Component {
-    state = {
-        counters: [
-            { id: 1, value: 0 },
-            { id: 2, value: 0 },
-            { id: 3, value: 0 },
-            { id: 4, value: 0 }
-        ]
-    };
-
-    handleDelete = (counterId) => {
-        const counters = this.state.counters.filter(c => c.id !== counterId);
-        this.setState({ counters: counters });
-    }
-
+class Counters extends Component {
     render() {
+        const { onReset, counters, onIncrement, onDelete } = this.props;
+
         return (
             <div>
-                {this.state.counters.map(counter =>
+                <button
+                    onClick={onReset}
+                    className="btn btn-primary btn-sm m-2">Reset</button>
+                {counters.map(counter =>
                     <Counter
                         key={counter.id}
-                        onDelete={this.handleDelete}
+                        onIncrement={onIncrement}
+                        onDelete={onDelete}
                         counter={counter}
                     />
                 )}
