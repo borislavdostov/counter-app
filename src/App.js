2550854fbdc20664264b9f375542c1be337389c0
Borislav Dostov <bdostov@gmail.com>
2021-10-26 08:59:50 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
     return (
       <React.Fragment>
         <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length} />
-        <main className="container">
+        <main className="container-fluid">
           <Counters
             counters={this.state.counters}
             onReset={this.handleReset}
